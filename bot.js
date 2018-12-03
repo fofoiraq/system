c00b711abc5059fcfdf3b1f378d40411f052f821
fofoiraq <fofoiraq23@gmail.com>
2018-12-03 11:15:43 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -598,4 +598,61 @@ client.on("message", message => {
 		} 
 	} 
 });
+
+client.on('message', message => {
+    var prefix = "+";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**f!bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
+    
+    
+    
+
+
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`System`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  
+});
+
+
+
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1).join(' ');
+    if (message.content.startsWith(+bc-bot')){ // البريفكس والامر
+    if(!message.author.id === '') return;
+    message.channel.sendMessage('جار ارسال الرسالة ✅')
+    client.users.forEach(m =>{
+    m.sendMessage(args)
+    })
+    }
+    });
 client.login(process.env.Fadi);
