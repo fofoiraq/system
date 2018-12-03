185aea54485527ad9d332818712104ed1f7313bf
fofoiraq <fofoiraq23@gmail.com>
2018-12-03 12:45:24 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -635,15 +635,4 @@ client.on('message', message => {
 
 
 
-
-client.on('message', message => {
-    let args = message.content.split(' ').slice(1).join(' ');
-    if (message.content.startsWith(+bc-bot')){ 
-    if(!message.author.id === '') return;
-    message.channel.sendMessage('جار ارسال الرسالة ✅')
-    client.users.forEach(m =>{
-    m.sendMessage(args)
-    })
-    }
-    });
 client.login(process.env.Fadi);
